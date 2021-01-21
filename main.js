bf1f24d2c93ffc912d3422ba6504da9fc18ca50d
caldorbee <calvinbeene@gmail.com>
2021-01-21 18:43:33 -0500

    changed price font weight

--- main.js
+++ main.js
@@ -3,11 +3,21 @@ const products = items.results
 let productsHtml = products.map (function (item) {
     return `
         <div class="product-item">
-            <img src="${item.Images[0].url_fullxfull}">
-            <h2>${item.title}</h2>
-            <strong>${item.price}</strong>
+            <div class=product-bg style="background-image: url(${item.Images[0].url_fullxfull})"></div>
+            <h2 class="title">${item.title}</h2>
+            <p class="title">${item.Shop.shop_name}</p>
+        <div id="rating">
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+            <span class="fa fa-star"></span>
+        </div>
+            <strong id="item-price">$${item.price}</strong>
         </div>
         `
-})
+}).join('')
+
+console.log (products[0])
 
 document.querySelector('#grid').innerHTML = productsHtml
