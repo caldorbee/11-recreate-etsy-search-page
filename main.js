2be8290254b53990068a53de9b1934cf5d53ef2b
caldorbee <calvinbeene@gmail.com>
2021-01-23 16:16:16 -0500

    added filters

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const products = items.results
 
 let productsHtml = products.map (function (item) {
-    return `
-        <div class="product-item">
+    return `<div class="product-item">
             <div class=product-bg style="background-image: url(${item.Images[0].url_fullxfull})"></div>
             <h2 class="title">${item.title}</h2>
             <p class="title">${item.Shop.shop_name}</p>
@@ -20,10 +19,37 @@ let productsHtml = products.map (function (item) {
 
 document.querySelector('#grid').innerHTML = productsHtml
 
-let navHtml = `<h1 id="etsy">Etsy</h1>
-             <div id="search-bar">
-                id="search-bar">
-                <input type="image" src="/_i/mag.png" alt="Search">
+let navHtml = `<nav id="nav-bar">
+                <h1 id="etsy">Etsy</h1>
+                <div type="text" id="search-bar">
+                    <input type="image" <i class="fa fa-search" aria-hidden="true"></i></input>
+                    </div>
+                <button id="sign-in" style="border:none;">Sign In</button>
+                <button id="cart" class="fa fa-shopping-cart"></button>
+            </nav>
+            <div id="dropdown">
+                <ul id="categories">
+                    <li><a id="valentine" href="#">Valentine's Day<li>
+                    <li><a id="jewelry" href="#">Jewelry & Accessories<li>
+                    <li><a id="clothing" href="#">Clothing & Shoes<li>
+                    <li><a id="home" href="#">Home & Living<li>
+                    <li><a id="wedding" href="#">Wedding & Party<li>
+                    <li><a id="toys" href="#">Toys & Entertainment<li>
+                    <li><a id="arts" href="#">Arts & Collectibles<li>
+                    <li><a id="craft" href="#">Craft Supplies<li>
+                    <li><a id="gift" href="#">Gifts & Gift Cards<li>
+                </ul>
+            <div>
+                <button class="filters" id="arrival">Estimated Arrival
+                </button>
+                <button class="filters" id="filter">All filters</button>
+                <button class="filters" id="save">heart</button>
+                <button class="filters" id="sort">sort</button>
+            </div>
             </div>`
 
 document.querySelector('#nav').innerHTML = navHtml
+
+// let footerHtml = `<footer>
+//                     <span></span>
+//                 </footer>`
